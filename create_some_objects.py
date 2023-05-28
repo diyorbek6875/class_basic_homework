d475e1e6916a35062f38aad7432d8193e284e666
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-29 00:30:51 +0500

    4

--- create_some_objects.py
+++ create_some_objects.py
@@ -1,4 +1,5 @@
 from create_one_attribute import Person
-
+p1=Person('Anvar')
+p2=Person('Shavkat')
 #create an object named "p1" whose name is "Anvar"
 #create an object named "p2" whose name is "Shavkat"
