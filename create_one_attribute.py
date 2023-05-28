a3671f20eb4bc5b239afabb67068dc88069b7092
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-29 00:17:03 +0500

    2

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,8 +1,5 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
 class Person:
-    # def __init__(self, name):
-    #     self.name1=name
     name="diyor"
 print(Person.name)
-# print(a.name1.capitalize)
