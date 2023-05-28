699d5ea6149abf5981239777bb9f3986c89be8af
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-29 01:01:44 +0500

    2

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,5 +1,8 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
 class Person:
-    name="Ali"
-print(Person.name)
+    def __init__(self,name) -> None:
+        self.name=name
+
+Ali=Person('Ali')
+print(type(Ali))
